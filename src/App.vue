7eefe3ebb507eb4d8a7a66500fb1499313bdaabc
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-23 14:05:37 -0300

    Contador em vue js -> decrementa e incrementa

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
-<script setup>
-import { ref } from 'vue' 
-const contador = ref(0)
+<script>
+/**export default {
+  data() {
+    return {
+      contador: 0
 
-function decrementar() {
-  contador.value--
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++
+    },
+    decrementarContador() {
+      if (this.contador > 0) {this.contador--}
+      else {this.contador = 0}
+    }
+  },
+  mounted() {
+    console.log(`O valor do contador inicial é ${this.contador}`)
+  }
 }
+**/
+</script>
+<script setup>
+import { ref, onMounted } from 'vue'
+const contador = ref(0)
+function incrementarContador() {contador.value++}
+function decrementarContador() {
+  if (contador.value > 0) contador.value-- 
+  else contador.value = 0
+}
+onMounted(() => {
+  console.log(`Contador incrementado: ${contador.value}`)
+  }
+
+)
 </script>
  
 <template>
-  <button @click="contador++">+</button>
-  <button @click="decrementar()">-</button>
+  <button @click="incrementarContador">+</button>
+  <button @click="decrementarContador">-</button>
   
   <p>Valor contador: {{ contador }}</p>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+</style>
