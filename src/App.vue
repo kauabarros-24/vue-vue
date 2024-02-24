69e5d43735a005101a0f2e22f163743730e3c7d7
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-24 19:32:44 -0300

    usamos interpolação de texto para inverter e exibir mensagens na tela com vueJs

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,15 @@
-<script>
-/**export default {
-  data() {
-    return {
-      contador: 0
 
-    }
-  },
-  methods: {
-    incrementarContador() {
-      this.contador++
-    },
-    decrementarContador() {
-      if (this.contador > 0) {this.contador--}
-      else {this.contador = 0}
-    }
-  },
-  mounted() {
-    console.log(`O valor do contador inicial é ${this.contador}`)
-  }
-}
-**/
-</script>
 <script setup>
-import { ref, onMounted } from 'vue'
-const contador = ref(0)
-function incrementarContador() {contador.value++}
-function decrementarContador() {
-  if (contador.value > 0) contador.value-- 
-  else contador.value = 0
+const mensagem = 'programar'
+function reverter(texto) {
+  return texto.split('').reverse('').join('')
 }
-function resetar() {
-  if (contador.value >= 1) {
-    contador.value = 0
-
-  }
-}
-onMounted(() => {
-  console.log(`Contador incrementado: ${contador.value}`)
-  }
-
-)
 </script>
  
 <template>
-  <button @click="incrementarContador">+</button>
-  <button @click="decrementarContador">-</button>
-  <button @click="resetar">Resetar</button>
+  <p>{{ reverter(mensagem) }}</p>
   
-  <p>Valor contador: {{ contador }}</p>
+
 </template>
 
 
