c4d9deee5109ad9e30c99eb1f18363802ffc50a6
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-08 14:07:14 -0300

    reatividade -> aula 3 de Dev.WEB II

--- src/App.vue
+++
@@ -1,3 +0,0 @@
-<script setup>function mostrarAlert() { alert("Botão clicaco")}</script>
-<template><a v-on:click="mostrarAlert"><b>Clique aqui</b></a></template>
-<style>a {cursor: pointer;}</style>
