f11046c799dbaa4a9935215edf049375de1ab0b0
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-08 15:17:56 -0300

    exercícios terminados

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script setup>
-    import { reactive } from 'vue';
+    import { ref} from 'vue';
+    const contador = ref(0);
+    const contador1 = ref(0)
+    function incrementar() {contador.value++;}
+    function decrementar() {if (contador.value >= 0) {contador.value--;}}
+    function incrementar2() {contador1.value++;}
+    function decrementar2() {if (contador1.value >= 0){contador1.value--;}}
+    const soma = contador1 + contador;
+    function resposta() {(soma % 2 == 0) ? document.write('O valor é par') : document.write('O valor é ímpar');}
+    const booleana = true;
+    function testeBoolean(){(booleana == true) ? document.write("Verdadeiro") : document.write("Falso")}
 
-    const pessoa = reactive({
-        nome: 'Kauã',
-        idade: 16,
-        ativo: true,
-    });
-    function aniversario() {pessoa.idade++}
 </script>
 <template>
     <div>
-        <p @click="aniversario()">O {{pessoa.nome}} tem {{ pessoa.idade }} anos </p>
+        <p> {{ contador + contador1}}</p>
+        <button @click="incrementar">Clique para incrementar o primeiro valor</button>
+        <button @click="decrementar">Clique para decrementar o primeiro valor </button>
+        <button @click="incrementar2">Clique para incrementar o primeiro valor</button>
+        <button @click="decrementar2">Clique para decrementar o primeiro valor </button>
+        <div v-if="soma > 10 || soma < 10">A sua soma excedeu ou precedeu o valor 10</div>
+        <div v-bind="resposta"></div>
+        <div v-bind="testeBoolean"></div>    
     </div>
+
 </template>
+
+
+
+
+ 
